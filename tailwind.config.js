936bcdad9dcdf800c5527c52091cfe471ef1272d
백지윤 <jy.baek079@gmail.com>
2024-07-03 00:36:08 +0900

    chore : 불필요한 파일 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
-    extend: {
-      fontFamily: {
-        WantedSansRegular: ['WantedSansRegular', 'sans-serif'],
-        WantedSansBlack: ['WantedSansBlack', 'sans-serif'],
-        WantedSansBold: ['WantedSansBold', 'sans-serif'],
-        WantedSansExtraBlack: ['WantedSansExtraBlack', 'sans-serif'],
-        WantedSansExtraBold: ['WantedSansExtraBold', 'sans-serif'],
-        WantedSansMedium: ['WantedSansMedium', 'sans-serif'],
-        WantedSansSemiBold: ['WantedSansSemiBold', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
