39357cda7a1e8d35b6666af58bf36331042197f5
백지윤 <jy.baek079@gmail.com>
2024-07-09 00:03:48 +0900

    design : 배경 설정, 사이드 패널 IT/과학 탭, article에서만 열리게 설정, 팝업 아이콘 누르면 바로 사이드패널 열리게 설정

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,11 +1,9 @@
-import reloadOnUpdate from "virtual:reload-on-update-in-background-script";
-import "webextension-polyfill";
+import reloadOnUpdate from 'virtual:reload-on-update-in-background-script';
+import 'webextension-polyfill';
 
-reloadOnUpdate("pages/background");
+reloadOnUpdate('pages/background');
 
-/**
- * Extension reloading is necessary because the browser automatically caches the css.
- * If you do not use the css of the content script, please delete it.
- */
+console.log('background loaded');
 
-console.log("background loaded");
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error));
