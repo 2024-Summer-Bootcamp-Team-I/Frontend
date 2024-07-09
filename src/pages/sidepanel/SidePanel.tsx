1ff77119761ed8d48bfc8492d5705666f836b02c
백지윤 <jy.baek079@gmail.com>
2024-07-09 15:08:20 +0900

    design : 회원가입 사이드패널 퍼블리싱

--- src/pages/sidepanel/SidePanel.tsx
+++ src/pages/sidepanel/SidePanel.tsx
@@ -3,17 +3,19 @@ import '@src/global.css';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import bgSidePanel from '@src/assets/img/bg_sidepannel.svg';
+import Signup from './Signup';
 
 const SidePanel = () => {
   return (
     <div
-      className="w-screen h-screen bg-bottom bg-no-repeat bg-cover"
+      className="flex justify-center w-screen h-screen bg-bottom bg-no-repeat bg-cover"
       style={{
         backgroundImage: `url(${bgSidePanel})`,
       }}
     >
-      <h1>My navernews sidepanel</h1>
-      <p>This side panel will display only on https://news.naver.com/</p>
+      <div className="flex items-center">
+        <Signup />
+      </div>
     </div>
   );
 };
---
+++ src/pages/sidepanel/Signup.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '@root/src/assets/img/logo.png';
+import usernameicon from '@root/src/assets/img/usernameicon.svg';
+import emailicon from '@root/src/assets/img/emailicon.svg';
+import passwordicon from '@root/src/assets/img/passwordicon.svg';
+
+const Signup = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <img src={logo} alt="Logo" className="w-[5rem] h-[1.5rem] mb-[5.5rem]" />
+      <div className="space-y-[1.5rem]">
+        <div className="flex items-center w-[24rem] h-[3.75rem] bg-white rounded-full border border-gray-400">
+          <input type="text" placeholder="UserName" className="w-[17.5rem] mx-[1.5rem] text-base focus:outline-none" />
+          <img src={usernameicon} alt="UsernameIcon" />
+        </div>
+        <div className="flex items-center w-[24rem] h-[3.75rem] bg-white rounded-full border border-gray-400">
+          <input type="text" placeholder="Email" className="w-[17.5rem] mx-[1.5rem] text-base focus:outline-none" />
+          <img src={emailicon} alt="EmailIcon" />
+        </div>
+        <div className="flex items-center w-[24rem] h-[3.75rem] bg-white rounded-full border border-gray-400">
+          <input type="text" placeholder="Password" className="w-[17.5rem] mx-[1.5rem] text-base focus:outline-none" />
+          <img src={passwordicon} alt="PasswordIcon" />
+        </div>
+      </div>
+      <button className="w-[12.5rem] h-[3.75rem] mt-[3rem] bg-midnight rounded-full text-xl text-white">
+        가입하기
+      </button>
+    </div>
+  );
+};
+
+export default Signup;
