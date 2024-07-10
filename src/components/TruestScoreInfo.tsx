13cfd2aa438a7bb64d7f59cc22777f2cc336463f
백지윤 <jy.baek079@gmail.com>
2024-07-10 23:57:31 +0900

    design: 피드백 사이드패널 퍼블리싱, 별점 구현

--- src/components/TruestScoreInfo.tsx
+++ src/components/TruestScoreInfo.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
-const Info = () => {
+const TruestScoreInfo = () => {
   return (
     <div>
       <ol className="flex flex-col space-y-4 text-base font-semibold">
         <li className="flex items-center space-x-4">
           <div className="w-[1.25rem] h-[1.25rem] drop-shadow-lg rounded-full ring-2 ring-white bg-gradient-to-r from-[#379FFF] to-[#647CF9]"></div>
-          <span>76 ~ 100</span>
+          <span className="leading-5">76 ~ 100</span>
         </li>
         <li className="flex items-center space-x-4">
           <div className="w-[1.25rem] h-[1.25rem] drop-shadow-lg rounded-full ring-2 ring-white bg-gradient-to-r from-[#96CDFF] to-[#00E6B9]"></div>
-          <span>51 ~ 75</span>
+          <span className="leading-5">51 ~ 75</span>
         </li>
         <li className="flex items-center space-x-4">
           <div className="w-[1.25rem] h-[1.25rem] drop-shadow-lg rounded-full ring-2 ring-white bg-gradient-to-r from-[#E6EB00] to-[#EBC500]"></div>
-          <span>26 ~ 50</span>
+          <span className="leading-5">26 ~ 50</span>
         </li>
         <li className="flex items-center space-x-4">
           <div className="w-[1.25rem] h-[1.25rem] drop-shadow-lg rounded-full ring-2 ring-white bg-gradient-to-r from-[#DA4E00] to-[#B30000]"></div>
-          <span>26 ~ 50</span>
+          <span className="leading-5">1 ~ 25</span>
         </li>
       </ol>
     </div>
   );
 };
 
-export default Info;
+export default TruestScoreInfo;
--- src/pages/sidepanel/Scrap.tsx
+++ src/pages/sidepanel/Scrap.tsx
@@ -1,25 +1,27 @@
 import ScrapBoard from '@root/src/components/ScrapBoard';
 import ScrapIcon from '@root/src/assets/img/ScrapIcon.svg';
 import line from '@src/assets/img/Line1.svg';
-import backGroundSide from '@src/assets/img/bg_sidepannel.svg';
 import BlankBoard from '@root/src/components/BlankBoard';
+import ShortcutIcon from '@src/assets/img/ShortcutsIcon.svg';
 
 const Scrap = () => {
   return (
     <div className="justify-center w-[30rem] h-[100vh]">
       <div className="flex h-[7.25rem] items-center justify-between">
         <p className="mx-8 h-[3rem] text-[2rem]">LOGO</p>
-        <div className="flex mx-4 items-center justify-center w-[14rem] h-[3rem] bg-[#113d66] text-white text-[1.25rem] rounded-full">
-          저장한 기사 보러가기
-        </div>
+        <button className="flex mx-8 items-center justify-center w-[14rem] h-[3rem] bg-midnight text-white text-[1.25rem] rounded-full">
+          <p className="pr-2">저장한 기사 보러가기</p>
+          <img src={ShortcutIcon} />
+        </button>
       </div>
-      <div className="text-[#113D66]">
+      <div className="text-midnight">
         <div className="px-[2rem] flex items-center">
           <img src={ScrapIcon} />
           <p className="ml-1 font-extrabold text-[1.5rem]">스크랩</p>
         </div>
         <p className="px-[2rem] py-[0.75rem] text-[1.25rem]">
-          저장하고 싶은 뉴스를 이 패널로 드래그하면 뉴스 기사가 저장됩니다.
+          저장하고 싶은 뉴스를 이 패널로 드래그하면 <br />
+          뉴스 기사가 저장됩니다.
         </p>
       </div>
       <div className="px-8 pt-[1.25rem] pb-[0.75rem]">
