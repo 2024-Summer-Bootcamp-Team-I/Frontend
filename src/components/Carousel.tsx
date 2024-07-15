0d311a4d973723dfde1f1026dc3b93bc7653d285
NightStar96 <dnenghfkddl2@gmail.com>
2024-07-16 03:53:26 +0900

    design:캐러셀 반응형 수정

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,13 +4,13 @@ import 'swiper/css';
 import 'swiper/css/mousewheel';
 import 'swiper/css/scrollbar';
 import 'swiper/css/effect-coverflow';
-import { Mousewheel, Scrollbar, EffectCoverflow } from 'swiper/modules';
+import { Mousewheel, Scrollbar, EffectCoverflow, FreeMode } from 'swiper/modules';
 import SavedNews from './SavedNews';
 
-const Carousel = () => {
+const Carousel: React.FC = () => {
   const swiperRef = useRef(null);
 
-  // 페이지 전체 스크롤 방지
+  // 페이지 스크롤 방지
   useEffect(() => {
     const originalStyle = window.getComputedStyle(document.body).overflow;
     document.documentElement.style.overflow = 'hidden';
@@ -25,9 +25,11 @@ const Carousel = () => {
   // 마우스 휠 이벤트
   useEffect(() => {
     const handleWheel = (e) => {
+      e.preventDefault();
       if (swiperRef.current && swiperRef.current.swiper) {
-        swiperRef.current.swiper.mousewheel.enable();
-        swiperRef.current.swiper.mousewheel.handle(e);
+        const swiper = swiperRef.current.swiper;
+        swiper.mousewheel.enable();
+        swiper.mousewheel.handle(e);
       }
     };
 
@@ -38,68 +40,66 @@ const Carousel = () => {
     };
   }, []);
 
-  // 슬라이드 데이터 갯수
+  // 슬라이드 데이터 설정
   const newsData = Array.from({ length: 10 }, (_, index) => ({ id: index + 1 }));
 
-  // 슬라이드 변경 시 슬라이드 크기 및 투명도 업데이트
+  // 슬라이드 변경 시 슬라이드 투명도 및 크기 업데이트
   const updateSlideOpacityAndSize = (swiper) => {
     const slides = swiper.slides;
     slides.forEach((slide, index) => {
       slide.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
       if (index === swiper.activeIndex) {
         slide.style.opacity = '1';
-        slide.style.transform = 'w-[65rem] h-[15rem]'; // 활성 슬라이드 크기 증가
+        slide.style.transform = 'scale(1)';
       } else if (index === swiper.activeIndex - 1 || index === swiper.activeIndex + 1) {
-        slide.style.opacity = '0.7';
-        slide.style.transform = 'w-[60rem] h-[10rem]'; // 인접한 슬라이드 크기 증가
-        // slide.style.transform = 'scale(0.8)';
+        slide.style.opacity = '0.8';
+        slide.style.transform = 'scale(0.7)';
       } else if (index === swiper.activeIndex - 2 || index === swiper.activeIndex + 2) {
-        slide.style.opacity = '0.5';
-        slide.style.transform = 'w-[55rem] h-[5rem]'; // 인접한 슬라이드 크기 증가
-        // slide.style.transform = 'scale(0.5)'; // 그 다음 슬라이드 기본 크기
+        slide.style.opacity = '0.6';
+        slide.style.transform = 'scale(0.5)';
       } else {
-        slide.style.opacity = '0';
-        slide.style.transform = 'scale(0)'; // 그 외 슬라이드 크기 감소
+        slide.style.opacity = '0.4';
+        slide.style.transform = 'scale(0.3)';
       }
     });
   };
 
-  // 슬라이드 클릭시 뭘 할건지
   const handleSlideClick = (index) => {
     console.log(`Slide ${index} clicked`);
+    // 슬라이드 클릭 동작
   };
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen overflow-hidden">
-      <div className="relative w-full h-full" style={{ width: '66.75rem', height: '49.5rem' }}>
+    <div className="flex overflow-hidden justify-center items-center">
+      <div className="relative w-[66.75rem] h-[47rem] 3xl:w-[75rem] 3xl:h-[52rem] 4xl:w-[85rem] 4xl:h-[55rem]">
         <style>{`
           .swiper-scrollbar {
             background: transparent;
-            position: absolute;
           }
           .swiper-scrollbar-drag {
             background: black;
           }
         `}</style>
         <Swiper
-          style={{ paddingBottom: '15rem' }} // 인라인 스타일로 marginBottom 추가
           ref={swiperRef}
           direction="vertical"
-          modules={[Mousewheel, Scrollbar, EffectCoverflow]}
+          modules={[Mousewheel, Scrollbar, EffectCoverflow, FreeMode]}
           mousewheel={{ forceToAxis: true, sensitivity: 1, releaseOnEdges: true }}
           effect="coverflow"
           coverflowEffect={{
             rotate: 0,
             stretch: 0,
+            depth: 200,
             modifier: 1,
             slideShadows: false,
           }}
-          slidesPerView={1.9}
+          slidesPerView={5}
           centeredSlides={true}
           scrollbar={{ draggable: true }}
           simulateTouch={false}
+          freeMode={{ enabled: false }}
           speed={300}
-          className="h-full mb-20"
+          className="h-full justify-center items-center"
           onSlideChange={updateSlideOpacityAndSize}
           onSwiper={updateSlideOpacityAndSize}
         >
@@ -108,9 +108,10 @@ const Carousel = () => {
               key={news.id}
               className="flex justify-center items-center"
               onClick={() => handleSlideClick(index)}
-              style={{ paddingTop: '14.7rem' }}
             >
-              <SavedNews />
+              <div>
+                <SavedNews />
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
