99b66409b4715dc833c4d26b39c3673c27f14262
백지윤 <jy.baek079@gmail.com>
2024-07-24 03:16:59 +0900

    feat: c전체 조회

--- src/components/CardNews.tsx
+++ src/components/CardNews.tsx
@@ -4,36 +4,51 @@ import calendarIcon from '@assets/img/Calendar.svg';
 import LiquidFillGauge from './LiquidFillGauge';
 import { useNavigate } from 'react-router-dom';
 
-const CardNews: React.FC = () => {
+type CardNewsProps = {
+  article: {
+    newsId: number;
+    title: string;
+    content: string;
+    img: string;
+    publishedDate: string;
+    channelName: string;
+    score: number;
+  };
+};
+
+const CardNews: React.FC<CardNewsProps> = ({ article }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate(`/maintextc/1`); // 1은 예시로 주어진 id입니다. 실제로는 동적으로 설정할 수 있습니다.
+    navigate(`/maintextc/${article.newsId}`);
   };
   return (
     <div
       onClick={handleClick}
       className="w-[26rem] 3xl:w-[35rem] 4xl:w-[47.375rem] h-[11.5rem] 3xl:h-[12rem] 4xl:h-[16rem] bg-white rounded-[1.875rem] shadow-md cursor-pointer"
     >
-      <div className="w-[26rem] 3xl:w-[35rem] 4xl:w-[47.375rem] h-[4.625rem] 3xl:h-[5.125rem] 4xl:h-[9.125rem] bg-gray-500 rounded-ss-[1.875rem] rounded-se-[1.875rem]" />
+      <img
+        src={article.img}
+        alt={article.title}
+        className="w-[26rem] 3xl:w-[35rem] 4xl:w-[47.375rem] h-[4.625rem] 3xl:h-[5.125rem] 4xl:h-[9.125rem] rounded-ss-[1.875rem] rounded-se-[1.875rem] object-cover"
+      />
+
       <div className="flex items-center justify-between">
         <div className="flex flex-col m-5 w-[18.25rem] 3xl:w-[27.25rem] 4xl:w-[39.625rem] h-[4.375rem] ">
-          <p className="flex-1 items-start text-[1rem] font-semibold">
-            닭 대신 미생물이 낳은 달걀…‘머랭’까지 칠 수 있다
-          </p>
+          <p className="flex-1 items-start text-[1rem] font-semibold">{article.title}</p>
           <div className="flex items-end justify-end">
             <div className="flex items-center ml-5">
               <img src={tvIcon} className="w-[0.75rem] h-[0.75rem]" />
-              <span className="ml-2 text-[0.75rem] text-[#505050]">연합뉴스</span>
+              <span className="ml-2 text-[0.75rem] text-[#505050]">{article.channelName}</span>
             </div>
             <div className="flex items-center ml-5">
               <img src={calendarIcon} className="w-[0.75rem] h-[0.75rem]" />
-              <span className="ml-2 text-[0.75rem] text-[#505050]">2024-07-04</span>
+              <span className="ml-2 text-[0.75rem] text-[#505050]">{article.publishedDate}</span>
             </div>
           </div>
         </div>
         <div className="m-5 ml-0 w-[4rem] h-[4rem]">
-          <LiquidFillGauge elementId={1} value={70} />
+          <LiquidFillGauge elementId={article.newsId} value={article.score} />
         </div>
       </div>
     </div>
--- src/pages/newtab/RelatedNews.tsx
+++ src/pages/newtab/RelatedNews.tsx
@@ -1,25 +1,69 @@
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import Pagination from '@mui/material/Pagination';
 import CardNews from '@root/src/components/CardNews';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const ClassifiedNews: React.FC = () => {
-  const emptyCards = Array.from({ length: 9 - CardNews.length });
-  console.log(emptyCards);
+const itemsPerPage = 9; // 페이지당 항목의 개수를 9로 설정
+
+const fetchRelatedNews = async (page: number) => {
+  const response = await axios.get(`http://localhost/api/v1/classifies/`, {
+    params: { page },
+  });
+  console.log(response.data);
+  return response.data;
+};
+
+const RelatedNews: React.FC = () => {
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['classifiedNews', page],
+    queryFn: () => fetchRelatedNews(page),
+  });
+
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  // 빈 카드를 계산하는 부분
+  const news =
+    data?.results.map((newsItem) => ({
+      newsId: newsItem.news.news_id,
+      title: newsItem.news.title,
+      content: newsItem.news.content,
+      img: newsItem.news.img,
+      publishedDate: newsItem.news.published_date,
+      channelName: newsItem.channel_name,
+      score: newsItem.score,
+    })) ?? [];
+  const emptyCards = Array.from({ length: itemsPerPage - news.length });
 
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center justify-center w-full h-full bg-opacity-10 bg-white rounded-[3.125rem] shadow-md p-8 3xl:px-10 4xl:p-16 mx-[3rem]">
         <div className="grid grid-cols-3 gap-x-8 gap-y-4 3xl:gap-y-8">
-          <CardNews />
+          {news.map((newsItem) => (
+            <CardNews key={newsItem.newsId} article={newsItem} />
+          ))}
           {emptyCards.map((_, index) => (
             <div
-              key={index}
+              key={`empty-${index}`}
               className="w-[26rem] 3xl:w-[35rem] 4xl:w-[47.375rem] h-[11.5rem] 3xl:h-[12rem] 4xl:h-[16rem]"
             ></div>
           ))}
         </div>
-        <div className="flex justify-center w-[15rem] h-[2rem] bg-red-500 mt-8 3xl:mt-10 4xl:mt-16" />
+        <div className="flex justify-center mt-8 3xl:mt-10 4xl:mt-16">
+          <Pagination count={data.total_pages} page={page} onChange={handlePageChange} color="primary" />
+        </div>
       </div>
     </div>
   );
 };
 
-export default ClassifiedNews;
+export default RelatedNews;
